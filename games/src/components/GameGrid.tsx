c6ae746d04f43ac7f4b73321bce9d8bf1fbc8242
Nhi-Luong <124530499+Nhi-Luong@users.noreply.github.com>
2024-09-16 17:19:25 -0700

    Create generic hook

--- games/src/components/GameGrid.tsx
+++ games/src/components/GameGrid.tsx
@@ -4,14 +4,14 @@ import GameCard from "./GameCard";
 import CardSkeleton from "./CardSkeleton";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGame();
+  const { data, error, isLoading } = useGame();
   const skeleton = [1,2,3,4,5,6];
   return (
     <>
       {error && <p>{error}</p>}
       <SimpleGrid columns={{sm:1, md:2, lg:3, xl:5}} padding={10} spacing={10}>
         {isLoading&&skeleton.map((k)=><CardSkeleton key={k}/>)}
-        {games.map((g) => (
+        {data.map((g) => (
           <GameCard game={g} />
         ))}
       </SimpleGrid>
